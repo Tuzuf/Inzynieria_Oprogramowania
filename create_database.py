2076a15cb94efc5121725962e9cbf4baf50775b1
Durawix <durawix.2k@gmail.com>
2025-01-04 14:46:04 +0100

    create chroma db

--- create_database.py
+++ create_database.py
@@ -1,4 +1,5 @@
 # from langchain.document_loaders import DirectoryLoader
+
 from langchain_community.document_loaders import DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.schema import Document
